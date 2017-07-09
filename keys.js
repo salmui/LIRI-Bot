a99726467bd621ae77e42bed456d8f810dbb88df
salmui_ <salhuang22@gmail.com>
2017-07-09 11:52:02 -0700

    node homework

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'xzdv4b4O3KycE0IvtXoJ8RuPN',
+  consumer_secret: 'QLunU9vshXP4Y1igAn9LDm0FMWOVQzSflIVUgt1WnMLZpvkRFo',
+  access_token_key: '128776858-zb5sUjwNCZ0euuSfQv3lwXRSSDzB67D8qrhHVTHo',
+  access_token_secret: 'ROqzJIt3qpxDQJDtRX5tNWXm6DeEFtOnRQOXo8bf7BrGW',
+}
+
+
+exports.spotifyKeys = {
+  id: 'f40df44e606f47b28519221385610730',
+  secret: '5fe7fbd2162a45b9b8bbb677babd52b8',
+}
---
+++ liri.js
@@ -0,0 +1,133 @@
+// Node module imports
+// reads file system package
+var fs = require("fs");
+var request = require("request");
+var keys = require("./keys.js");
+var twitter = require ("twitter");
+var client = new twitter(keys.twitterKeys);
+var spotify = require ("node-spotify-api");
+var command = process.argv[2];
+var nodeArgv = process.argv;
+
+//movie or song
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<nodeArgv.length; i++){
+  if(i>3 && i<nodeArgv.length){
+    x = x + "+" + nodeArgv[i];
+  } else{
+    x = x + nodeArgv[i];
+  }
+}
+
+// Commands with switch case
+switch(command) {
+  case "my-tweets":
+    myTweets();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+
+  default:
+    console.log("{node liri.js + : my-tweets, spotify-this-song, movie-this, do-what-it-says}")
+    break;
+}
+
+  function myTweets(){
+    //Display last 20 Tweets
+    var twitterUsername = { user_name: 'salmui_'};
+    client.get('statuses/user_timeline/', twitterUsername, function(error, tweets, response){
+      if(!error){
+        for(var i = 0; i < tweets.length; i++){
+          console.log(response);
+          var date = tweets[i].created_at;
+          console.log("@salmui_: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+          console.log("-----------------------");
+        }
+      }else{
+        console.log('Error');
+      }
+    });
+  }
+
+// Spotify function
+
+    var spotify = new Spotify({
+      id: mySpotifyKeys.id,
+      secret: mySpotifyKeys.secret
+    });
+
+	function spotifyThisSong(songName) {
+		var songName = process.argv[3];
+		if(!songName){
+			songName = "Dilemma";
+		}
+		song = songName;
+		spotify.search({ type: "track", query: song }, function(err, data) {
+			if(!err){
+				var songInfo = data.tracks.items[i];
+				for (var i = 0; i < data.tracks.items.length; i++) {
+					if (songInfo[i] != undefined) {
+						var spotifyResults =
+						("Artist: " + songInfo[i].artists[0].name);
+						("Song: " + songInfo[i].name);
+						("Album: " + songInfo[i].album.name);
+						("Preview Url: " + songInfo[i].preview_url);
+						console.log(spotifyResults);
+					}
+				}
+			}	else {
+				console.log("Error :"+ err);
+				return;
+			}
+		});
+	};
+
+  // Movie function, uses the Request module to call the OMDB api
+function movieThis(){
+  var movie = process.argv[3];
+  if(!movie){
+    movie = "Wonder Woman";
+  }
+  params = movie
+  request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var movieObject = JSON.parse(body);
+      //console.log(movieObject)
+      var movieResults =
+      "Title: " + movieObject.Title
+      "Year: " + movieObject.Year
+      "Imdb Rating: " + movieObject.imdbRating
+      "Country: " + movieObject.Country
+      "Language: " + movieObject.Language
+      "Plot: " + movieObject.Plot
+      "Actors: " + movieObject.Actors
+      "Rotten Tomatoes Rating: " + movieObject.tomatoRating
+      "Rotten Tomatoes URL: " + movieObject.tomatoURL
+
+      console.log(movieResults);
+      log(movieResults); // calling log function
+    } else {
+      console.log("Error :"+ error);
+      return;
+    }
+  });
+};
+	// Do What It Says function
+	function doWhatItSays() {
+		fs.readFile("random.txt", "utf8", function(error, data){
+			if (!error) {
+				doWhatItSaysResults = data.split(",");
+				spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+			} else {
+				console.log("Error occurred" + error);
+			}
+		});
+	};
